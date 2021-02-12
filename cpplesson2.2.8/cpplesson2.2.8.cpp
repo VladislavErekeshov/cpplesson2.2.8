cb10fbf651d2ebb2436c6b21166e6f9a08c0819d
VladislavErekeshov <tommysyching@gmail.com>
2021-02-12 22:15:45 +0300

    Добавьте файлы проекта.

---
+++ cpplesson2.2.8/cpplesson2.2.8.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+	int first = a * b * c * c + a * b * b * c + a * b * c * c;
+	cout << first << endl;
+	int second = 3 * a * a - 2 * b * b * b + 4 * c * c;
+	cout << second << endl;
+	int third = 5 * a * b + a * a * b * b * c * c - 6;
+	cout << third << endl;
+	cout << first * first - 4 * second * second + third * third;
+	return 0;
+}
